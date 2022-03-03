940668a04c00a5d29624de72f0e16916df3e72e9
Xinxink <83113427+Xinxink@users.noreply.github.com>
2022-03-03 20:11:16 +0800

    Update text.vue

--- docs/examples/button/text.vue
+++ docs/examples/button/text.vue
@@ -1,4 +1,4 @@
 <template>
-  <el-button type="text">Text Button</el-button>
+  <el-button type="large">Text Button</el-button>
   <el-button type="text" disabled>Text Button</el-button>
 </template>
